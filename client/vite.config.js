7e4a479da84277c28e8b7861b54e5a9b54dd221c
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-03-20 22:07:16 +0200

    chore: added .env variable for server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 // Відтворюємо __dirname для ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // __dirname
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  // cors handling
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        // __dirname
+        '@': path.resolve(__dirname, './src'),
       },
-    }
-  }
+    },
+    // cors handling
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
