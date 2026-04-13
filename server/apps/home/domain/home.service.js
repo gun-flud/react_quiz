a75672c8afcabbc711faeb253fa2ffcaa0ac958a
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-13 22:42:49 +0300

    fix(server): fixed data sending

--- server/apps/home/domain/home.service.js
+++ server/apps/home/domain/home.service.js
@@ -1,6 +1,10 @@
 import { dataAccessModule } from "../data-access/home.repository.js";
 
- export const data = await dataAccessModule.findAll();
+//  export const data = await dataAccessModule.findAll();
+
+const value = await dataAccessModule.findAll();
+
+export const data = value.quizzes;
 
 
 // console.log(data);
