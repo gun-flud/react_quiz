d1d737b29b6524f4275832e34f541325666b42c3
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-25 12:00:00 +0300

    chore(db): add npm run migrate script to package.json

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -1,5 +1,7 @@
 import { config } from "dotenv";
-config({ path: `.env.${process.env.NODE_ENV || 'development'}`});
+config({ 
+    path: `.env.${process.env.NODE_ENV || "development"}`, 
+    debug: true });
 
 import * as z from "zod";
 
@@ -9,7 +11,7 @@ import * as z from "zod";
 
 // for (envVar of envVariables) {
 
-//     if(!process.env[envVar]) { 
+//     if(!process.env[envVar]) {
 //         console.error('problem with environment variable: ', envVar);
 //         process.exit(1);
 //     }
@@ -23,19 +25,20 @@ const envSchema = z.object({
     PORT: z.coerce.number(),
     DB_HOST: z.coerce.string(),
     DB_PORT: z.coerce.number(),
-DB_PORT_INTERNAL: z.coerce.number(),
-DB_NAME: z.coerce.string(),
-DB_USER: z.coerce.string(),
-DB_PASSWORD: z.coerce.string(),
+    DB_PORT_INTERNAL: z.coerce.number(),
+    DB_NAME: z.coerce.string(),
+    DB_USER: z.coerce.string(),
+    DB_PASSWORD: z.coerce.string(),
 
-PGADMIN_DEFAULT_EMAIL: z.coerce.string(),
-PGADMIN_DEFAULT_PASSWORD: z.coerce.string(),
+    PGADMIN_DEFAULT_EMAIL: z.coerce.string(),
+    PGADMIN_DEFAULT_PASSWORD: z.coerce.string(),
+    PGADMIN_PORT: z.coerce.number(),
 });
 const result = envSchema.safeParse(process.env);
 
 if (!result.success) {
     console.error("Invalid environment variables:", result.error.format());
     process.exit(1);
-} 
+}
 
 export const env = result.data;
