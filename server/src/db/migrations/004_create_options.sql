7012a11f3a175e6e49f839a96ef167dd54283530
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-27 20:55:41 +0300

    feat(db): add 004_create_options.sql — options for each quiz with foreign key to questions

---
+++ server/src/db/migrations/004_create_options.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS options (
+    id UUID UNIQUE
+        DEFAULT gen_random_uuid(),
+    question_id UUID NOT NULL,
+    body VARCHAR(500) NOT NULL,
+    is_correct BOOLEAN
+        DEFAULT false,
+    position INT,
+
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (question_id) REFERENCES questions(id) ON DELETE CASCADE
+)
