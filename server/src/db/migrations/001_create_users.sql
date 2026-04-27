10209fa855637926b8095a2f3e3826afc5abc396
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-27 20:52:06 +0300

    feat(db): add 001_create_users.sql — users table with email, password, role

---
+++ server/src/db/migrations/001_create_users.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID UNIQUE
+        DEFAULT gen_random_uuid(),
+    user VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(400) NOT NULL,
+    email VARCHAR(60),
+    profile_photo VARCHAR(255),
+    profile_description TEXT,
+
+    PRIMARY KEY (id)
+)
