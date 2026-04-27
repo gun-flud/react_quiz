abb271ceb2d6f488ab73e0c70d3c6dbb81abbf1d
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-04-27 20:53:31 +0300

    feat(db): add 002_create_quizzes.sql — quizzes table with foreign key to users

---
+++ server/src/db/migrations/002_create_quizzes.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS quizzes (
+    id UUID UNIQUE 
+        DEFAULT gen_random_uuid(), 
+    creatior_id UUID NOT NULL, 
+    bg_image TEXT, 
+    title VARCHAR(255) NOT NULL, 
+    description TEXT, 
+    public BOOLEAN NOT NULL 
+        DEFAULT false, 
+    points INT 
+        DEFAULT NULL,
+
+    PRIMARY KEY (id)
+    FOREIGN KEY (creatior_id) REFERENCES users(id) ON DELETE CASCADE
+    )
