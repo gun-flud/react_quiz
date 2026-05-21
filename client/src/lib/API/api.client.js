2f3409a911fab556da2666b5735b7f9308c208af
vladIvannikov <ivannikovvladislav1@gmail.com>
2026-05-22 01:36:55 +0300

    feat(auth-proxy): created auth proxy for client's auth handling

--- client/src/lib/API/api.client.js
+++ client/src/lib/API/api.client.js
@@ -1,3 +1,41 @@
+// export default async function apiClient (path, options={}) {
+//     try {
+//         const {headers: headersOpt, ...resOptions} = options;
+
+//         const hasBody = options.body !== undefined;
+
+//         const response = await fetch(path, {
+//             ...resOptions,
+//             credentials: "include",
+//             headers: {
+//                 ...(hasBody && {'Content-Type': 'application/json;charset=utf-8'}),
+//                 ...headersOpt, // all headers 
+//             },        
+//         });
+
+//         if (!response.ok) {
+//             throw new Error("Response status:", response.status);
+//         }
+
+//         const result = await response.json();
+//         return result; 
+//     } catch (err) {
+//         if (err.name !== "AbortError") {
+//             console.error('res error:', err.message || err);
+//         }
+//         throw err;
+//     }
+// }
+
+export class ApiError extends Error {
+    constructor (code, error) {
+        super(code);
+        this.erData = error;
+    }
+}
+
+
+
 export default async function apiClient (path, options={}) {
     try {
         const {headers: headersOpt, ...resOptions} = options;
@@ -14,11 +52,9 @@ export default async function apiClient (path, options={}) {
         });
 
         if (!response.ok) {
-            throw new Error("Response status:", response.status);
-        }
+            const errorVal = await response.json().catch(() => ({}));
 
-        if (response.status === 401) {
-             console.warn("User is unauthorized. Redirecting to login...");
+            throw ApiError(response.status, errorVal);
         }
 
         const result = await response.json();
---
+++ client/src/lib/API/proxies/authProxy.js
@@ -0,0 +1,65 @@
+import apiClient, { ApiError } from "../api.client.js";
+
+let accessToken = null;
+let failedQueue = [];
+let isRefreshing = false;
+
+export const setAccessToken = (token) => {
+    accessToken = token;
+};
+
+export default async function AuthProxy(path, options = {}) {
+    try {
+        const headers = new Headers(options.headers);
+
+        if (accessToken) {
+            headers.set("Authorization", `Bearer ${accessToken}`);
+        }
+
+        options = { ...options, headers };
+        const response = await apiClient(path, options);
+
+        if (response.accessToken) {
+            setAccessToken(response.accessToken);
+        }
+
+        return response;
+    } catch (error) {
+        if (error instanceof ApiError && error.status === 401) {
+            if (error.erData && error.erData.code === "TOKEN_EXPIRED") {
+
+                if (isRefreshing) {
+                    return new Promise((resolve, reject) => {
+                       failedQueue.push({resolve, reject}); 
+                    }).then(() => AuthProxy(path, options))
+                    .catch((err) => Promise.reject(err));
+                }
+
+                isRefreshing = true;
+
+                try {
+                    const refreshPath = "api/auth/refresh";
+                    const refreshData = await apiClient(refreshPath, { method: "POST" });
+                    
+                    setAccessToken(refreshData.accessToken);
+
+                    failedQueue.forEach((promise) => promise.resolve());
+                    failedQueue = [];
+
+                    return await AuthProxy(path, options);
+
+                } catch (refreshError) {
+                    failedQueue.forEach((prom) => prom.reject(refreshError));
+                    failedQueue = [];
+                    setAccessToken(null);
+                    // window.location.href = "/login";
+                    throw refreshError;
+                    
+                } finally {
+                    isRefreshing = false;
+                }
+            }
+        }
+        throw error;
+    }
+}
